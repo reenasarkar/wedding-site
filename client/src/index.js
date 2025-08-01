5363fda9f693c1dcd6f26e1490af283f0a304598
Reena Sarkar <reenasarkar@Reenas-MacBook-Pro-2.local>
2025-08-01 12:13:16 -0400

    try to fix font again

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Font loading check
+if ('fonts' in document) {
+  document.fonts.ready.then(() => {
+    console.log('Fonts loaded successfully');
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
