e9044c7c1f9a95f028c8e433e9c8f3cf8744ecd7
Reena Sarkar <reenasarkar@Reenas-MacBook-Pro-2.local>
2025-08-05 17:54:41 -0400

    workin w event planner

--- client/src/Schedule.jsx
+++ client/src/Schedule.jsx
@@ -7,7 +7,8 @@ export default function Schedule() {
       <img src={logo} className="page-app-logo" alt="logo" />
 
       <h1>Event Schedule</h1>
-      <p>Coming Soon</p>
+      <p className="schedule-coming-soon">Coming Soon!</p>
+      <p className="schedule-description">I am working on this with the event planner and will send out an email blast when this info is ready, so you can know more details 😜</p>
     </div>
   )
 }
