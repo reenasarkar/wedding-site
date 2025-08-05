9e5cf4fbc70df0ee5add3d489991ea70bc5730e7
Reena Sarkar <reenasarkar@Reenas-MacBook-Pro-2.local>
2025-08-05 18:18:22 -0400

    forgot both jays

--- server/load_guests.py
+++ server/load_guests.py
@@ -114,6 +114,8 @@ GUEST_LIST = [
     {"name": "Vivek Verma", "email": "", "plus_one_allowed": False, "associated_guest": "Vandana Verma"},
     {"name": "Shiva Sood", "email": "", "plus_one_allowed": False, "associated_guest": ""},
     {"name": "Alvin Modin", "email": "", "plus_one_allowed": True, "associated_guest": ""},
+    {"name": "Jay Gorsia", "email": "", "plus_one_allowed": False, "associated_guest": ""},
+    {"name": "Jay Chen", "email": "", "plus_one_allowed": False, "associated_guest": ""},
     {"name": "Testy Boobs", "email": "test@test.com", "plus_one_allowed": False, "associated_guest": "Daenerys Stormborn of the House Targaryen"},
 ]
 
